af96c3c5452a783eeb9216ca6b194644a90f6d84
mahdimortuza <mahdi.webx@gmail.com>
2024-05-24 10:38:37 +0600

    server done

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,35 @@ import cors from "cors";
 import express, { Request, Response } from "express";
 const app = express();
 const port = 5000;
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 // parsers
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173" }));
-// app.use(cors({ origin: "https://ocean-catch-client.vercel.app" }));
 
-const response = "Hello World! It's yummyfy.";
+// MongoDB Connection URL
+const uri = process.env.DATABASE_URL;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const db = client.db("yummyFy");
+    const recipeCollection = db.collection("recipe");
+  } catch (error) {
+    console.log(error);
+  }
+}
+run().catch(console.dir);
+
+const response = "Hello World! It's yummyFy.";
 app.get("/", (req: Request, res: Response) => {
   res.send(response);
 });
